76ae7ecf7d2726c69895bb3f820061c302a1f797
Palash Jyoti Goswami <palashjyotigoswamipt@gmail.com>
2022-07-25 18:35:08 +0530

    day1 commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:stopwatch/watch.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +11,60 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          backgroundColor: Colors.white,
+          body: SafeArea(
+            child: Column(
+              children: [
+                const Expanded(
+                  child: Image(
+                    image: AssetImage('assets/image1.png'),
+                  ),
+                ),
+                const Expanded(
+                  child: Watch(),
+                ),
+                Expanded(
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: GestureDetector(
+                          onTap: () {},
+                          child: const Icon(
+                            CupertinoIcons.restart,
+                            color: Colors.black,
+                            size: 25,
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: GestureDetector(
+                          onTap: () {},
+                          child: const Icon(
+                            CupertinoIcons.play_fill,
+                            color: Colors.blueAccent,
+                            size: 50,
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: GestureDetector(
+                          onTap: () {},
+                          child: const Icon(
+                            CupertinoIcons.flag,
+                            color: Colors.black,
+                            size: 25,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )),
+    );
   }
 }
